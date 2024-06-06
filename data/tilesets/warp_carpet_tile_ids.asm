4879cb79688e411091bb7f6a916b45cf7141e4c5
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-06-06 13:12:37 +0200

    Update warp_carpet_tile_ids.asm
    update comments

--- data/tilesets/warp_carpet_tile_ids.asm
+++ data/tilesets/warp_carpet_tile_ids.asm
@@ -18,7 +18,7 @@ ENDM
 	warp_carpet_tiles $01, $5C
 
 .FacingLeftWarpTiles:
-	warp_carpet_tiles $1A, $4B, $12, $17 ; marcelnote - test, added $12, $17
+	warp_carpet_tiles $1A, $4B, $12, $17 ; marcelnote - added $12, $17 for updated Route16
 
 .FacingRightWarpTiles:
-	warp_carpet_tiles $0F, $4E, $5C, $5A ; marcelnote - test, added $5C, $5A
+	warp_carpet_tiles $0F, $4E, $5C, $5A ; marcelnote - added $5C, $5A for updated Route16
