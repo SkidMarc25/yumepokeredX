6f31a7a7ae25ff88bab871ba9c3fa2242784d5ab
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-06-29 22:55:34 +0200

    Update MandarinSilphCo.asm
    Added warps

--- data/maps/objects/MandarinSilphCo.asm
+++ data/maps/objects/MandarinSilphCo.asm
@@ -8,6 +8,12 @@ MandarinSilphCo_Object:
 	def_warp_events
 	warp_event  4, 49, LAST_MAP, 5
 	warp_event  5, 49, LAST_MAP, 5
+	warp_event  6, 42, MANDARIN_SILPH_CO, 4 ; stairs 1F -> 2F
+	warp_event  6, 28, MANDARIN_SILPH_CO, 3 ; stairs 2F -> 1F
+	warp_event  7, 28, MANDARIN_SILPH_CO, 6 ; stairs 2F -> 3F
+	warp_event  7, 14, MANDARIN_SILPH_CO, 5 ; stairs 3F -> 2F
+	warp_event  6, 14, MANDARIN_SILPH_CO, 8 ; stairs 3F -> 4F
+	warp_event  6,  0, MANDARIN_SILPH_CO, 7 ; stairs 4F -> 3F
 
 	def_bg_events
 	;bg_event  4,  9, TEXT_CELADONMANSION1F_MANAGERS_SUITE_SIGN
