751dac03df57f5c1afab58560c8da13db6d49e6b
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-08-02 17:17:11 +0900

    Pokeyellow engine for sprites
    Imported engine to display sprites from pokeyellow to allow spriteset switch from overworld maps
    - overworld/map_sprites.asm file
    - new function CopyVideoDataAlternate from pokeyellow
    
    Route 19 sprites updates
    - changed female swimmers to new sprite
    - modified sprite itself

--- home/copy.asm
+++ home/copy.asm
@@ -22,3 +22,24 @@ CopyData::
 	or b
 	jr nz, CopyData
 	ret
+
+; marcelnote - new from pokeyellow
+CopyVideoDataAlternate::
+	ldh a, [rLCDC]
+	bit 7, a ; LCD enabled?
+	jp nz, CopyVideoData ; if yes, then copy video data
+	push hl
+	ld h, d
+	ld l, e
+	pop de
+	ld a, b ; save bank
+	push af
+	swap c
+	ld a, $f
+	and c
+	ld b, a
+	ld a, $f0
+	and c
+	ld c, a
+	pop af
+	jp FarCopyData
