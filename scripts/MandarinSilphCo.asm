dd89488b3f0903a11fd10695fc9d141396ff3771
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-08-02 17:18:12 +0900

    Mandarin Silph Co

--- scripts/MandarinSilphCo.asm
+++ scripts/MandarinSilphCo.asm
@@ -4,13 +4,49 @@ MandarinSilphCo_Script:
 
 MandarinSilphCo_TextPointers:
 	def_text_pointers
-	;dw_const CeladonMansion1FGrannyText,            TEXT_CELADONMANSION1F_GRANNY
-	;dw_const CeladonMansion1FManagersSuiteSignText, TEXT_CELADONMANSION1F_MANAGERS_SUITE_SIGN
+	dw_const MandarinSilphCo1FReceptionistText,        TEXT_MANDARINSILPHCO1F_RECEPTIONIST_TEXT
+	dw_const MandarinSilphCo2FWorkerFText,             TEXT_MANDARINSILPHCO2F_WORKER_F_TEXT
+	dw_const MandarinSilphCo2FWorkerMText,             TEXT_MANDARINSILPHCO2F_WORKER_M_TEXT
+	dw_const MandarinSilphCo2FAssistantText,           TEXT_MANDARINSILPHCO2F_ASSISTANT_TEXT
+	dw_const MandarinSilphCo3FScientistText,           TEXT_MANDARINSILPHCO3F_SCIENTIST_TEXT
+	dw_const MandarinSilphCo4FManagerText,             TEXT_MANDARINSILPHCO4F_MANAGER_TEXT
+	dw_const MandarinSilphCo2FSalesSignText,           TEXT_MANDARINSILPHCO2F_SALES_SIGN
+	dw_const MandarinSilphCo3FMeetingRoomSignText,     TEXT_MANDARINSILPHCO3F_MEETING_ROOM_SIGN
+	dw_const MandarinSilphCo4FRegionalManagerSignText, TEXT_MANDARINSILPHCO4F_REGIONAL_MANAGER_SIGN
 
-;CeladonMansion1FGrannyText:
-;	text_far _CeladonMansion1FGrannyText
-;	text_end
 
-;CeladonMansion1FManagersSuiteSignText:
-;	text_far _CeladonMansion1FManagersSuiteSignText
-;	text_end
+MandarinSilphCo1FReceptionistText:
+	text_far _MandarinSilphCo1FReceptionistText
+	text_end
+
+MandarinSilphCo2FWorkerFText:
+	text_far _MandarinSilphCo2FWorkerFText
+	text_end
+
+MandarinSilphCo2FWorkerMText:
+	text_far _MandarinSilphCo2FWorkerMText
+	text_end
+
+MandarinSilphCo2FAssistantText:
+	text_far _MandarinSilphCo2FAssistantText
+	text_end
+
+MandarinSilphCo3FScientistText:
+	text_far _MandarinSilphCo3FScientistText
+	text_end
+
+MandarinSilphCo4FManagerText:
+	text_far _MandarinSilphCo4FManagerText
+	text_end
+
+MandarinSilphCo2FSalesSignText:
+	text_far _MandarinSilphCo2FSalesSignText
+	text_end
+
+MandarinSilphCo3FMeetingRoomSignText:
+	text_far _MandarinSilphCo3FMeetingRoomSignText
+	text_end
+
+MandarinSilphCo4FRegionalManagerSignText:
+	text_far _MandarinSilphCo4FRegionalManagerSignText
+	text_end
--- text/MandarinSilphCo.asm
+++ text/MandarinSilphCo.asm
@@ -1,13 +1,57 @@
 ; marcelnote - new location
-_MandarinSilphCoGrannyText::
-	text "My dear #MON"
-	line "keep me company."
 
-	para "MEOWTH even brings"
-	line "money home!"
+; PLACEHOLDER TEXT FOR NOW
+_MandarinSilphCo1FReceptionistText::
+	text "Welcome to the"
+	line "MANDARIN ISLAND"
+	cont "branch of"
+	cont "SILPH CO."
 	done
 
-_MandarinSilphCoManagersSuiteSignText::
-	text "CELADON MANSION"
-	line "Manager's Suite"
+_MandarinSilphCo2FWorkerFText::
+	text "What is a kid"
+	line "doing here?"
+	done
+
+_MandarinSilphCo2FWorkerMText::
+	text "So much work"
+	line "these days."
+	done
+
+_MandarinSilphCo2FAssistantText::
+	text "I am the assistant"
+	line "regional manager!"
+	done
+
+_MandarinSilphCo3FScientistText::
+	text "I am waiting for"
+	line "a meeting with"
+	cont "the regional"
+	cont "manager to"
+	cont "discuss current"
+	cont "issues at the"
+	cont "factory."
+	done
+
+
+_MandarinSilphCo4FManagerText::
+	text "Hello!"
+	line "I am the manager"
+	cont "of SILPH CO's"
+	cont "MANDARIN branch."
+	done
+
+_MandarinSilphCo2FSalesSignText::
+	text "SILPH CO"
+	line "Sales"
+	done
+
+_MandarinSilphCo3FMeetingRoomSignText::
+	text "SILPH CO"
+	line "Meeting room"
+	done
+
+_MandarinSilphCo4FRegionalManagerSignText::
+	text "SILPH CO"
+	line "Regional manager"
 	done
