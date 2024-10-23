56a2bd0ba580e6aa06a8e67362cd0d3f548f2fb2
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-10-23 11:16:19 +0200

    Update to Game Corner prizes

--- data/events/prizes.asm
+++ data/events/prizes.asm
@@ -12,46 +12,53 @@ PrizeMenuMon1Entries:
 IF DEF(_RED)
 	db NIDORINA
 ENDC
-IF DEF(_BLUE)
+IF DEF(_BLUE) || DEF(_GREEN) ; marcelnote - added _GREEN
 	db NIDORINO
 ENDC
 	db "@"
 
-PrizeMenuMon1Cost:
-IF DEF(_RED)
-	bcd2 180
-	bcd2 500
-ENDC
-IF DEF(_BLUE)
+PrizeMenuMon1Cost: ; marcelnote - harmonized costs
 	bcd2 120
-	bcd2 750
-ENDC
+	bcd2 500
+;IF DEF(_RED)
+;	bcd2 180
+;	bcd2 500
+;ENDC
+;IF DEF(_BLUE)
+;	bcd2 120
+;	bcd2 750
+;ENDC
 	bcd2 1200
 	db "@"
 
-PrizeMenuMon2Entries:
+PrizeMenuMon2Entries: ; marcelnote - modifications to prizes
+; marcelnote - PINSIR and SCYTHER are now more common, so made cheaper than DRATINI
+;              and swap them to increase accessibility in the version where they are rarer
 IF DEF(_RED)
-	db DRATINI
+	;db DRATINI
+	;db SCYTHER
+	db PINSIR
+ENDC
+IF DEF(_BLUE) || DEF(_GREEN) ; marcelnote - added _GREEN
+	;db PINSIR
+	;db DRATINI
 	db SCYTHER
 ENDC
-IF DEF(_BLUE)
-	db PINSIR
-	db DRATINI
-ENDC
+	db DRATINI  ; marcelnote - DRATINI now second in all versions
 	db PORYGON
 	db "@"
 
-PrizeMenuMon2Cost:
-IF DEF(_RED)
-	bcd2 2800
-	bcd2 5500
-	bcd2 9999
-ENDC
-IF DEF(_BLUE)
+PrizeMenuMon2Cost: ; marcelnote - harmonized costs
+;IF DEF(_RED)
+;	bcd2 2800
+;	bcd2 5500
+;	bcd2 9999
+;ENDC
+;IF DEF(_BLUE) || DEF(_GREEN) ; marcelnote - added _GREEN
 	bcd2 2500
 	bcd2 4600
 	bcd2 6500
-ENDC
+;ENDC
 	db "@"
 
 PrizeMenuTMsEntries:
