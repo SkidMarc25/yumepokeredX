32c2011148f3e93a58f679ca097826398fbe3314
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-10-26 03:45:07 +0200

    Added Paras to Route 2

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,20 +1,21 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  3, RATTATA
-	db  3, PIDGEY
+	;db  3, PIDGEY ; marcelnote - removed
 	db  4, PIDGEY
 	db  4, RATTATA
 	db  5, PIDGEY
+	db  3, PARAS ; marcelnote - added
 IF DEF(_RED)
 	db  3, WEEDLE
-	db  2, RATTATA
+	db  4, PARAS ; marcelnote - was 2, RATTATA
 	db  5, RATTATA
 	db  4, WEEDLE
 	db  5, WEEDLE
 ENDC
 IF (DEF(_BLUE) || DEF(_GREEN)) ; marcelnote: Green has same encounters as Blue for now
 	db  3, CATERPIE
-	db  2, RATTATA
+	db  4, PARAS ; marcelnote - was 2, RATTATA
 	db  5, RATTATA
 	db  4, CATERPIE
 	db  5, CATERPIE
