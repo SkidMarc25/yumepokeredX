83c1cf23f88b1b01232e6291475efd55886078a9
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-10-30 20:57:56 +0100

    Small edits

--- text/Route12FishingGuideHouse.asm
+++ text/Route12FishingGuideHouse.asm
@@ -3,9 +3,6 @@ _Route12FishingGuideHouseFishingGuideText::
 	text "This is a"
 	line "FISHING GUIDE!"
 
-	;para "<PLAYER> adds"
-	;line "it to the #DEX."
-
 	para "Oh, it is not"
 	line "written yet..."
 	done
