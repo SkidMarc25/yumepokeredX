24dac8516c11aa57f1222728ce1bf05f2f1b5b40
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-10-31 21:36:28 +0100

    Update trainer_card.asm

--- gfx/trainer_card.asm
+++ gfx/trainer_card.asm
@@ -1,8 +1,8 @@
 TrainerInfoTextBoxTileGraphics:  INCBIN "gfx/trainer_card/trainer_info.2bpp"
 TrainerInfoTextBoxTileGraphicsEnd:
 
-;BlankLeaderNames: INCBIN "gfx/trainer_card/blank_leader_names.2bpp"
-LeaderNames: INCBIN "gfx/trainer_card/leader_names.2bpp" ; marcelnote - added leader names on trainer card
+BlankLeaderNames: INCBIN "gfx/trainer_card/blank_leader_names.2bpp"
+;LeaderNames: INCBIN "gfx/trainer_card/leader_names.2bpp" ; marcelnote - added leader names on trainer card
 
 CircleTile: INCBIN "gfx/trainer_card/circle_tile.2bpp"
 
