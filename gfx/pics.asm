fb117f6dfc41d96d524cbdf7c23e8ba5d9a59f7e
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2024-11-25 23:06:24 +0100

    Fix Starmie backsprite bug

--- gfx/pics.asm
+++ gfx/pics.asm
@@ -261,10 +261,7 @@ PidgeottoPicBack::     INCBIN "gfx/pokemon/back/pidgeottob.pic"
 PidgeotPicFront::      INCBIN "gfx/pokemon/front/pidgeot.pic"
 PidgeotPicBack::       INCBIN "gfx/pokemon/back/pidgeotb.pic"
 StarmiePicFront::      INCBIN "gfx/pokemon/front/starmie.pic"
-
-RedPicBack::           INCBIN "gfx/player/redb.pic"
-GreenPicBack::         INCBIN "gfx/player/greenb.pic"
-OldManPicBack::        INCBIN "gfx/battle/oldmanb.pic"
+StarmiePicBack::       INCBIN "gfx/pokemon/back/starmieb.pic"
 
 
 SECTION "Pics 5", ROMX
@@ -323,7 +320,6 @@ WeepinbellPicFront::   INCBIN "gfx/pokemon/front/weepinbell.pic"
 WeepinbellPicBack::    INCBIN "gfx/pokemon/back/weepinbellb.pic"
 VictreebelPicFront::   INCBIN "gfx/pokemon/front/victreebel.pic"
 VictreebelPicBack::    INCBIN "gfx/pokemon/back/victreebelb.pic"
-StarmiePicBack::       INCBIN "gfx/pokemon/back/starmieb.pic"
 
 
 SECTION "Pics 6", ROMX
@@ -375,3 +371,7 @@ ChannelerPic::     INCBIN "gfx/trainers/channeler.pic"
 AgathaPic::        INCBIN "gfx/trainers/agatha.pic"
 LancePic::         INCBIN "gfx/trainers/lance.pic"
 YellowPic::        INCBIN "gfx/trainers/yellow.pic" ; marcelnote - new trainer class
+
+RedPicBack::           INCBIN "gfx/player/redb.pic"
+GreenPicBack::         INCBIN "gfx/player/greenb.pic"
+OldManPicBack::        INCBIN "gfx/battle/oldmanb.pic"
