795d034242db4c2078dbea5c8a1780c5666cd674
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-01-04 02:55:56 +0100

    Remove Circle tile

--- gfx/trainer_card.asm
+++ gfx/trainer_card.asm
@@ -4,6 +4,4 @@ TrainerInfoTextBoxTileGraphicsEnd:
 ; marcelnote - new, for printing leader names in Trainer card
 LeaderNames: INCBIN "gfx/trainer_card/leader_names.2bpp"
 
-CircleTile: INCBIN "gfx/trainer_card/circle_tile.2bpp"
-
 BadgeNumbersTileGraphics: INCBIN "gfx/trainer_card/badge_numbers.2bpp"
