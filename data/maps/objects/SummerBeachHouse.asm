efaeac46017b5b06c4361c2662cdd2acb418b6b1
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-04-19 12:50:30 +0200

    Beach house update
    - modified Beach House tileset
    - modified Beach House map
    - added Pikachu overworld sprite

--- data/maps/objects/SummerBeachHouse.asm
+++ data/maps/objects/SummerBeachHouse.asm
@@ -8,20 +8,20 @@
 	;const_export SUMMERBEACHHOUSE_PRINTER
 
 SummerBeachHouse_Object:
-	db $a ; border block
+	db $0 ; border block
 
 	def_warp_events
 	warp_event  2,  7, LAST_MAP, 1
 	warp_event  3,  7, LAST_MAP, 1
 
 	def_bg_events
-	bg_event  3,  0, TEXT_SUMMERBEACHHOUSE_POSTER1
+	bg_event  4,  2, TEXT_SUMMERBEACHHOUSE_POSTER1
 	bg_event  7,  0, TEXT_SUMMERBEACHHOUSE_POSTER2
-	bg_event 11,  0, TEXT_SUMMERBEACHHOUSE_POSTER3
+	bg_event 12,  0, TEXT_SUMMERBEACHHOUSE_POSTER3
 	;bg_event 13,  1, TEXT_SUMMERBEACHHOUSE_PRINTER
 
 	def_object_events
 	;object_event  2,  3, SPRITE_FISHING_GURU, STAY, DOWN, TEXT_SUMMERBEACHHOUSE_SURFINDUDE
-	object_event  5,  3, SPRITE_PIKACHU, WALK, 1, TEXT_SUMMERBEACHHOUSE_PIKACHU
+	object_event  2,  4, SPRITE_PIKACHU, WALK, ANY_DIR, TEXT_SUMMERBEACHHOUSE_PIKACHU
 
 	def_warps_to SUMMER_BEACH_HOUSE
