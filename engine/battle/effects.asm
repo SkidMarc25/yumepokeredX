f914f6cc8281c16243beb87314a205015b976cf6
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-05-12 13:24:38 +0200

    TriAttackEffect update

--- engine/battle/effects.asm
+++ engine/battle/effects.asm
@@ -1453,81 +1453,80 @@ TriAttackEffect:  ; marcelnote - new effect for TRI_ATTACK: 20% chance of PAR/BR
 	ld [wAnimationType], a
 	callfar CheckTargetSubstitute
 	ret nz ; can't cause a status on a substitute
-	ld bc, wEnemyMonStatus
-	ld hl, wEnemyMonStatus + 1
 	ldh a, [hWhoseTurn]
 	and a
-	jr z, .next
-	ld bc, wBattleMonStatus
-	ld hl, wBattleMonStatus + 1
-.next
-	ld a, [bc]
+	ld hl, wEnemyMonStatus
+	jr z, .playerTurn
+	ld hl, wBattleMonStatus
+.playerTurn
+	ld a, [hli] ; hl = w<>MonType1
 	and a ; does the target already have a status ailment?
 	ret nz
 	call BattleRandom ; generates a random number in a
 
-.burn
+	; burn
 	cp 17 ; 17/256 chance of result being 0-16 (6.6%)
 	jr nc, .freeze
-; check if the target is immune due to types
-	ld a, [hli]
+	; check if the target is immune due to types
+	ld a, [hli] ; a = [w<>MonType1]
 	cp FIRE
 	ret z
-	ld a, [hl]
+	ld a, [hld] ; a = [w<>MonType2]
 	cp FIRE
 	ret z
-	ld a, (1 << BRN)
-	ld [bc], a
-	callfar HalveAttackDueToBurn
+	dec hl ; hl = w<>MonStatus
+	ld [hl], 1 << BRN
+	call HalveAttackDueToBurn
+	ld hl, BurnedText
 	ldh a, [hWhoseTurn]
 	and a
-	jr nz, .skipBurnAnim
-	ld a, ENEMY_HUD_SHAKE_ANIM
-	call PlayBattleAnimation
-.skipBurnAnim
-	jp PrintBurnedText
+	jr z, .playEnemyHUDAnim
+	jr .print_text
 
 .freeze
 	cp 34 ; 17/256 chance of result being 17-33 (6.6%)
 	jr nc, .paralyze
 ; check if the target is immune due to types
-	ld a, [hli]
+	ld a, [hli] ; a = [w<>MonType1]
 	cp ICE
 	ret z
-	ld a, [hl]
+	ld a, [hld] ; a = [w<>MonType2]
 	cp ICE
 	ret z
-	ld a, (1 << FRZ)
-	ld [bc], a
-	callfar ClearHyperBeam
+	dec hl ; hl = w<>MonStatus
+	ld [hl], 1 << FRZ
+	call ClearHyperBeam
+	ld hl, FrozenText
 	ldh a, [hWhoseTurn]
 	and a
-	jr nz, .skipFreezeAnim
-	ld a, ENEMY_HUD_SHAKE_ANIM
-	call PlayBattleAnimation
-.skipFreezeAnim
-	jp PrintFrozenText
+	jr z, .playEnemyHUDAnim
+	jr .print_text
 
 .paralyze
 	cp 51 ; 17/256 chance of result being 34-50 (6.6%)
 	ret nc
 ; check if the target is immune due to types
-	ld a, [hli]
+	ld a, [hli] ; a = [w<>MonType1]
 	cp GROUND
 	ret z
-	ld a, [hl]
+	ld a, [hld] ; a = [w<>MonType2]
 	cp GROUND
 	ret z
-	ld a, (1 << PAR)
-	ld [bc], a
-	callfar QuarterSpeedDueToParalysis
+	dec hl ; hl = w<>MonStatus
+	ld [hl], 1 << PAR
+	call QuarterSpeedDueToParalysis
+	ld hl, ParalyzedMayNotAttackText
 	ldh a, [hWhoseTurn]
 	and a
-	jr nz, .skipParalyzeAnim
+	jr nz, .print_text
+	; fallthrough
+
+.playEnemyHUDAnim
 	ld a, ENEMY_HUD_SHAKE_ANIM
-	call PlayBattleAnimation
-.skipParalyzeAnim
-	jp PrintMayNotAttackText
+	call PlayBattleAnimation ; preserves hl
+.print_text
+	jp PrintText
+
 
 PayDayEffect:
 	jpfar PayDayEffect_
