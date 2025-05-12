6779d28971075c05473b5807bb48ba5b586f8281
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-05-12 18:55:48 +0200

    ConversionEffect update

--- engine/battle/move_effects/conversion.asm
+++ engine/battle/move_effects/conversion.asm
@@ -1,16 +1,16 @@
 ConversionEffect_:
-	ld hl, wEnemyMonType1
-	ld de, wBattleMonType1
 	ldh a, [hWhoseTurn]
 	and a
 	ld a, [wEnemyBattleStatus1]
-	jr z, .conversionEffect
+	ld hl, wEnemyMonType1
+	ld de, wBattleMonType1
+	jr z, .playerTurn
+	ld a, [wPlayerBattleStatus1]
 	push hl
 	ld h, d
-	ld l, e
-	pop de
-	ld a, [wPlayerBattleStatus1]
-.conversionEffect
+	ld l, e ; hl = wBattleMonType1
+	pop de  ; de = wEnemyMonType1
+.playerTurn
 	bit INVULNERABLE, a ; is mon immune to typical attacks (dig/fly)
 	jr nz, PrintButItFailedText
 ; copy target's types to user
