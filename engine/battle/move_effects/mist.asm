8663db529f71753fd467eab7920ce208c45b7440
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-05-19 20:44:51 +0200

    Update mist.asm

--- engine/battle/move_effects/mist.asm
+++ engine/battle/move_effects/mist.asm
@@ -1,17 +1,18 @@
 MistEffect_:
-	ld hl, wPlayerBattleStatus2
 	ldh a, [hWhoseTurn]
 	and a
-	jr z, .mistEffect
+	ld hl, wPlayerBattleStatus2
+	jr z, .gotPointer
 	ld hl, wEnemyBattleStatus2
-.mistEffect
+.gotPointer
 	bit PROTECTED_BY_MIST, [hl] ; is mon protected by mist?
-	jr nz, .mistAlreadyInUse
+	jr nz, .butItFailed
 	set PROTECTED_BY_MIST, [hl] ; mon is now protected by mist
 	callfar PlayCurrentMoveAnimation
 	ld hl, ShroudedInMistText
 	jp PrintText
-.mistAlreadyInUse
+
+.butItFailed
 	jpfar PrintButItFailedText_
 
 ShroudedInMistText:
