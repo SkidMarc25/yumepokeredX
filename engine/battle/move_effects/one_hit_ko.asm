135124bf8dbfbf5c72c36902d220922920a7db43
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-05-19 12:18:55 +0200

    OneHitKOEffect optim

--- engine/battle/move_effects/one_hit_ko.asm
+++ engine/battle/move_effects/one_hit_ko.asm
@@ -1,38 +1,39 @@
-OneHitKOEffect_:
-	ld hl, wDamage
-	xor a
-	ld [hli], a
-	ld [hl], a ; set the damage output to zero
-	dec a
-	ld [wCriticalHitOrOHKO], a
-	ld hl, wBattleMonSpeed + 1
-	ld de, wEnemyMonSpeed + 1
+OneHitKOEffect_: ; marcelnote - optimized
 	ldh a, [hWhoseTurn]
 	and a
+	ld hl, wBattleMonSpeed + 1
+	ld de, wEnemyMonSpeed + 1
 	jr z, .compareSpeed
 	ld hl, wEnemyMonSpeed + 1
 	ld de, wBattleMonSpeed + 1
 .compareSpeed
-; set damage to 65535 and OHKO flag is the user's current speed is higher than the target's
+; is the user slower than the target?
 	ld a, [de]
+	ld b, a     ; b = target's speed (low byte)
 	dec de
-	ld b, a
-	ld a, [hld]
+	ld a, [hld] ; a = user's speed   (low byte)
 	sub b
 	ld a, [de]
-	ld b, a
-	ld a, [hl]
-	sbc b
-	jr c, .userIsSlower
+	ld b, a     ; b = target's speed (high byte)
+	ld a, [hl]  ; a = user's speed   (high byte)
+	sbc b       ; sets carry if the user is slower
+
 	ld hl, wDamage
 	ld a, $ff
+	jr c, .userIsSlower
+; if user is not slower, set damage to 65535 and OHKO flag
 	ld [hli], a
 	ld [hl], a
 	ld a, $2
 	ld [wCriticalHitOrOHKO], a
 	ret
+
 .userIsSlower
-; keep damage at 0 and set move missed flag if target's current speed is higher instead
-	ld a, $1
+; if user is slower, set damage to 0, failed OHKO flag, and move missed flag
+	ld [wCriticalHitOrOHKO], a ; $ff = failed OHKO
+	xor a
+	ld [hli], a
+	ld [hl], a                 ; set the damage output to zero
+	inc a                      ; a = $1
 	ld [wMoveMissed], a
 	ret
