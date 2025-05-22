7af57e794f0f9cbd5baf88e7b2e03dee3dd4a7fa
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-05-22 11:25:32 +0200

    Add _DivAlt
    Alternate division function under testing

--- engine/math/multiply_divide.asm
+++ engine/math/multiply_divide.asm
@@ -134,3 +134,61 @@ _Divide:: ; marcelnote - adapted from polishedcrystal
 	ldh a, [c]
 	ldh [hQuotient], a
 	ret
+
+
+; This version takes low byte of dividend from [hDividend + 3] no matter b
+_DivAlt:: ; marcelnote - adapted from polishedcrystal
+; Divide hDividend length b (max 4 bytes) by hDivisor (1 byte). Result in hQuotient.
+; All values are big endian.
+	ldh a, [hDivisor]
+	and a ; is divisor 0?
+	ret z ; here Polished Crystal had an error crash
+
+	ld d, a              ; d = divisor
+	ld c, LOW(hDividend) ; to use ldh a, [c]
+	ld e, 0              ; e = remainder
+
+	ld b, 4              ; number of bytes to check
+.skipZeros
+	ldh a, [c]           ; a = [hDividend + nByte], next dividend byte
+	and a                ; is this dividend 0?
+	jr nz, .loopBytes    ; if not, start division
+	inc c                ; otherwise move to next byte
+	dec b
+	jr nz, .skipZeros
+; all dividends are 0 so set remainder to 0 and we're done
+	ldh [c], a           ; [hRemainder] = 0
+	ret
+
+.loopBytes
+	ldh a, [c]    ; a = [hDividend + nByte], next dividend byte
+	ld h, a       ; h = dividend for this byte
+	ld l, 0       ; l = quotient for this byte
+	ld b, 8       ; b = bit counter
+.loopBits
+	sla h
+	rl e          ; bring in next bit of h into e
+	ld a, e       ; a = remainder
+	jr c, .carry  ; if carry from rl e, then %1e [9bits] ≥ d so subtract d and increase l
+	cp d          ; e ≥ d?
+	jr c, .skip   ; if not, move to next bit
+.carry
+	sub d         ; subtract divisor from remainder
+	ld e, a       ; update remainder
+	inc l         ; increase quotient
+.skip
+	dec b         ; one less bit to do
+	jr z, .done
+	sla l         ; shift quotient left
+	jr .loopBits
+.done
+	ld a, l
+	ldh [c], a    ; [hQuotient + nByte] = quotient
+	inc c         ; move to next dividend byte
+	ld a, c
+	cp LOW(hRemainder) ; is c = LOW(hRemainder)?
+	jr nz, .loopBytes  ; if not, more bytes to divide
+
+	ld a, e
+	ldh [c], a    ; [hRemainder] = e
+	ret
