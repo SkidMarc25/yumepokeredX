ca60cfc1237b5abec9466b541c30d26636f15eed
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-05-24 16:47:12 +0200

    Update turn_sprite.asm

--- engine/overworld/turn_sprite.asm
+++ engine/overworld/turn_sprite.asm
@@ -1,4 +1,4 @@
-; marcelnote - this function was bugged as it loaded from [hCurrentSpriteOffset], which was always sprite $f0 = 15
+; marcelnote - this function was bugged as it loaded from [hCurrentSpriteOffset], which was always sprite 15 ($f0)
 ;              modified it to affect correct sprite
 UpdateSpriteFacingOffsetAndDelayMovement::
 	ld h, HIGH(wSpriteStateData2)
@@ -8,7 +8,7 @@ UpdateSpriteFacingOffsetAndDelayMovement::
 	ld l, a
 	ld a, $7f ; maximum movement delay
 	ld [hli], a ; x#SPRITESTATEDATA2_MOVEMENTDELAY
-    assert SPRITESTATEDATA1_FACINGDIRECTION == SPRITESTATEDATA2_MOVEMENTDELAY + 1
+    ASSERT SPRITESTATEDATA1_FACINGDIRECTION == SPRITESTATEDATA2_MOVEMENTDELAY + 1
 
 	dec h ; HIGH(wSpriteStateData1)
 	ld a, [hld] ; x#SPRITESTATEDATA1_FACINGDIRECTION
@@ -18,7 +18,7 @@ UpdateSpriteFacingOffsetAndDelayMovement::
 	ld [hl], a  ; x#SPRITESTATEDATA1_INTRAANIMFRAMECOUNTER
 	ld a, l
 	sub SPRITESTATEDATA1_INTRAANIMFRAMECOUNTER - SPRITESTATEDATA1_IMAGEINDEX
-	assert SPRITESTATEDATA1_INTRAANIMFRAMECOUNTER > SPRITESTATEDATA1_IMAGEINDEX
+	ASSERT SPRITESTATEDATA1_INTRAANIMFRAMECOUNTER > SPRITESTATEDATA1_IMAGEINDEX
 	ld l, a
 	ld a, [hl] ; x#SPRITESTATEDATA1_IMAGEINDEX
 	or b ; or in the facing direction
