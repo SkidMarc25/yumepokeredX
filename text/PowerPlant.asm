6f730305ae6b4ebdda53f1e69c977571481c49d3
Pokefan Marcel <166040321+PokefanMarcel@users.noreply.github.com>
2025-07-29 18:19:43 -0700

    Power plant text update

--- text/PowerPlant.asm
+++ text/PowerPlant.asm
@@ -9,8 +9,8 @@ _PowerPlantZapdosBattleText::
 _PowerPlantComputerText:: ; marcelnote - new
 	text "A damaged console."
 
-	para "Ouch! It gives"
-	line "a jolt."
+	para "Ouch! It crackles"
+	line "with static."
 	done
 
 _PowerPlantLiveWaterText:: ; marcelnote - new
